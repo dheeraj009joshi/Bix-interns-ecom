ac1f7c95cb416bce958ced5c78319a6fa19beda9
Shraddha <160100827+Shraddha318@users.noreply.github.com>
2024-09-11 22:27:55 +0530

    Update User.js

--- Models/User.js
+++ Models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -17,21 +16,11 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
-    isAdmin: {
-        type: Boolean,
-        default: false
-    },
+    role: { type: String, enum: ["Admin", "User"], default: "User" },
     createdOrders: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Order'
     }]
-});
-
-// Hashing the password before saving it to the database
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 12);
-    next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
