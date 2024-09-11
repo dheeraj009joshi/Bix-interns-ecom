d722dfd5d668c49b849cd9f5fea97e431937f840
Tanishq <77535859+tanishqtrivedi@users.noreply.github.com>
2024-09-11 21:33:38 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT || 3000,
-  dbUri: process.env.DB_URI || 'mongodb://localhost:27017/ecommerce',
+  dbUri: process.env.DB_URL || 'mongodb://localhost:27017/ecommerce',
   jwtSecret: process.env.JWT_SECRET || 'Shradha',
 };
 
