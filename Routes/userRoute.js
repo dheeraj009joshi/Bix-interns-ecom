77b7b4972cbd7a04a018c0bdceec892f10b8ce26
Shraddha <160100827+Shraddha318@users.noreply.github.com>
2024-09-13 12:11:11 +0530

    Update userRoute.js

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const { registerUser, loginUser, getCurrentUser} = require('../Controllers/userControllers');
+
+const protect = require('../middleware/auth');
+
+// Route to get all users and create a new user
+app.route('/')
+      .get(protect, getAllUsers)
+      .post(protect, createUser);
+
+// Route to get, update, and delete a user by ID
+app.route('/:id')
+      .get(protect, getUserById)
+      .put(protect, updateUser)
+      .delete(protect, deleteUser);
+
+module.exports = router;
 
