e5fdc1d731855679c3506a52cc3f2cdfdb939e5f
unknown <dlovej009@gmail.com>
2024-09-13 10:44:00 +0530

    dj

--- Controllers/OrderControl.js
+++ Controllers/OrderControl.js
@@ -1,3 +1,29 @@
+const { json } = require('body-parser');
 const Order = require('../Models/Order');
 const User= require('../Models/User');
+
+
+exports.createOder = async (req, res, next)=>{
+
+    try{
+
+        const oder=  await Order.create(req.body);
+        res.status(201).json( {success: true, data: oder})
+    }catch (error){
+        console.log.apply(error)
+        next(error) // sending error the handler
+    }
+   
+};
+
+exports.getOrderById = async (req, res, next)=>{
+    try{
+        const oder=  await Order.findById(req.params.id);
+        res.status(200).json( {success: true, data: oder, message:"Oder found"})
+    }catch (error){
+        console.log.apply(error)
+        next(error) // sending error the handler
+    }
+   
+};
 
--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+import {getOrderById,createOder} from "../Controllers/OrderControl" ;
 
-router.get('/', (req, res) => {
-  res.send('Order route');
-});
+
+app.route("/")
+      .get(protect,getAllOrders)
+      .post(protect, createOder)
+
+
+app.route("/:id")
+      .get(protect,getOrderById)
+      .patch(protect, deleteOrder)
+      .put(protect,updateOrder)
+      
+
+
+
 
 module.exports = router; 
