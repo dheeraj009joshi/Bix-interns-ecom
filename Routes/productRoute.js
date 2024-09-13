14c4b9f2d6b8b0ab1a593652e34ea39001cacb24
Shraddha <160100827+Shraddha318@users.noreply.github.com>
2024-09-13 11:44:02 +0530

    Update productRoute.js

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Product route');
-});
+const { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } = require('../Controllers/productControl');  // Import product controller methods
 
-module.exports = router; 
+const { protect } = require('../Middlewares/authMiddleware');  
+
+// Route to handle product creation and fetching all products
+app.route('/')
+      .get(protect, getAllProducts)   // Get all products 
+      .post(protect, createProduct);  // Create a new product
+
+// Route to handle operations on a specific product by ID
+app.route('/:id')
+      .get(protect, getProductById)   // Get a specific product by ID 
+      .patch(protect, updateProduct)  // Partially update product by ID 
+      .put(protect, updateProduct)    // Fully update a product by ID 
+      .delete(protect, deleteProduct); // Delete a product by ID 
+
+module.exports = router;
+
+
 
 
